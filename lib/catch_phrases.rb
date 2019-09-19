8d72169e69bc0c7f88239ffb4d184ea9912a744c
Roberta Flores <r.flores.1227@gmail.com>
2019-09-19 09:28:16 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -12,8 +12,9 @@ def link
 	puts 'It\'s Dangerous To Go Alone! Take This.'
 end
 
-def any_phrase(phrase)
+def any_phrase(phrase) do
 	phrase = "Do A Barrel Roll!"
+	puts phrase
 end
 
 any_phrase(phrase)
